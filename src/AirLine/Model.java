b8cd1c7058c1da9f1334e611fdb16aeb44120c1a
Abdelrahman-Osama0 <https://github.com/abdelrahman-osama0>
2023-12-15 17:56:27 +0200

    #1

---
+++ src/AirLine/Model.java
@@ -0,0 +1,13 @@
+package AirLine;
+
+public abstract class Model {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/AirLine/People.java
@@ -0,0 +1,11 @@
+package AirLine;
+
+public abstract class People extends Model {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+
+}
