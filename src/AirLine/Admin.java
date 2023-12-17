416e64305e9b7369811e21cc3c4b13ffe60fe0c5
Ahmedelsayed265 <https://github.com/Ahmedelsayed265>
2023-12-18 00:14:13 +0200

    #55

--- src/AirLine/Admin.java
+++ src/AirLine/Admin.java
@@ -1,13 +1,16 @@
 package AirLine;
 
-import java.util.Objects;
+import DataBase.DatabaseConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Admin extends People {
     private static int counter;
     private String emailAddress;
-
-    private boolean login;
-
+    private static boolean login;
     private String password;
 
     public Admin(String name, String emailAddress, String password) {
@@ -32,12 +35,70 @@ public class Admin extends People {
         this.password = password;
     }
 
-    public void login(String email, String password) {
-        login = Objects.equals(this.emailAddress, email) && Objects.equals(this.password, password);
+    // +++++++ Start Login Methods ++++++++//
+    public static boolean isLoggedIn() {
+        return login;
     }
 
-    public void register(String name, String email, String password) {
-
+    public static void login(String email, String password) {
+        try (Connection connection = DatabaseConnector.getConnection()) {
+            String query = "SELECT * FROM Admins WHERE email = ? AND password = ?";
+            try (PreparedStatement ps = connection.prepareStatement(query)) {
+                ps.setString(1, email);
+                ps.setString(2, password);
+                try (ResultSet res = ps.executeQuery()) {
+                    login = res.next();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    //add   bookFlight - updateFlight - displayFlight - deleteFlight
+    // +++++++ end Login Methods ++++++++//
+
+    // +++++++ Start Register Methods ++++++++//
+
+    private static boolean isEmailExists(Connection connection, String email) throws SQLException {
+        String query = "SELECT COUNT(*) FROM Admins WHERE email = ?";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, email);
+            try (ResultSet resultSet = ps.executeQuery()) {
+                if (resultSet.next()) {
+                    int count = resultSet.getInt(1);
+                    return count > 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidEmail(String email) {
+        return email != null && email.contains("@");
+    }
+
+    public static void register(String name, String email, String password) {
+        if (!isValidEmail(email)) {
+            System.out.println("Invalid email address");
+            return;
+        }
+        try (Connection connection = DatabaseConnector.getConnection()) {
+            if (isEmailExists(connection, email)) {
+                System.out.println("Email address is already registered");
+                return;
+            }
+            // String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+            String query = "INSERT INTO Admins (name, email, password) VALUES (?, ?, ?)";
+            try (PreparedStatement ps = connection.prepareStatement(query)) {
+                ps.setString(1, name);
+                ps.setString(2, email);
+                ps.setString(3, password);
+
+                ps.executeUpdate();
+                System.out.println("Hello " + name + ", your account registered successfully");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    // +++++++ end Register Methods ++++++++//
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import AirLine.Admin;
+import java.util.Scanner;
+
 public class Main {
+    static String email;
+    static String password;
+    static String name;
+
     public static void main(String[] args) {
+
+        System.out.println("1- already have an account? Login");
+        System.out.println("2- Don't have an account? Register");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Choice : ");
+        int ch = input.nextInt();
+
+        switch (ch) {
+            case 1:
+                do {
+                    System.out.print("Enter your email : ");
+                    email = input.next();
+                    System.out.print("Enter your password : ");
+                    password = input.next();
+
+                    Admin.login(email, password);
+                    if (Admin.isLoggedIn()) {
+                        System.out.println("Login successful!");
+                    } else {
+                        System.out.println("Login failed. Invalid email or password.");
+                    }
+                } while (!Admin.isLoggedIn());
+                break;
+
+            case 2:
+                System.out.print("Enter your name : ");
+                name = input.next();
+                System.out.print("Enter your email : ");
+                email = input.next();
+                System.out.print("Enter your password : ");
+                password = input.next();
+
+                Admin.register(name, email, password);
+                break;
+        }
 
     }
 }
